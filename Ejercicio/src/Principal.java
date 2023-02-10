8575cfb36fc20201a535fadb43ba25a1678ff1c0
Estudiante USC <estudiante@GTACAD.USC>
2023-02-10 16:25:06 -0500

    hola

--- Ejercicio/src/Principal.java
+++ Ejercicio/src/Principal.java
@@ -2,19 +2,25 @@ import java.util.Scanner;
 
 //nombre de la clase
 public class Principal {
-	
-	
-	
-	//metodo principal
+
+	// metodo principal
 	public static void main(String[] args) {
 		
-		System.out.print("Ingrese la palabra: ");
-		//Clase que permite capturar datos por consola
-		Scanner sc = new Scanner (System.in);
-		
-		
-		
-			String prueba = sc.next();
-			System.out.println("La cantidad de letras que tiene la palabra es: " + prueba.length() );
+		System.out.println(contarPalabras());
+
 	}
+
+	public static String contarPalabras() {
+
+		System.out.print("Ingrese la palabra: ");
+		// Clase que permite capturar datos por consola
+		Scanner sc = new Scanner(System.in);
+
+		String prueba = sc.next();
+		
+		return"La cantidad de letras que tiene la palabra es: " + prueba.length();
+		
+
+	}
+
 }
