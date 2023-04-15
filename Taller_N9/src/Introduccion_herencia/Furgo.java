057f7632752820843510d4c247d8b4c1990e4dc8
Jonathann1505 <125077741+Jonathann1505@users.noreply.github.com>
2023-04-15 17:40:56 -0500

    Taller_N°9

---
+++ Taller_N9/src/Introduccion_herencia/Furgo.java
@@ -0,0 +1,13 @@
+package Introduccion_herencia;
+
+public class Furgo   extends Coche{
+		private int capacidadCarga;
+		private int plazasExtras;
+		
+		public Furgo(int plazasExtras, int capacidadCarga) {
+			super();
+			
+			this.capacidadCarga = capacidadCarga;
+			this.plazasExtras = plazasExtras;
+		}
+	}
