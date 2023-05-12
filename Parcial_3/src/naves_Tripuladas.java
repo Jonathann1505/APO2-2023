238bdbfcaf9cafbc39104edeeb4653938d6cb7e2
Estudiante USC <estudiante@GTACAD.USC>
2023-05-12 16:25:17 -0500

    Parcial Final

---
+++ Parcial_3/src/naves_Tripuladas.java
@@ -0,0 +1,113 @@
+import java.util.List;
+
+public class naves_Tripuladas {
+	// Variables 
+	private String proposito;
+    private List<String> misiones;
+    private List<String> centros_investigacion;
+    private double peso;
+    private double distancia_orbita;
+    private int capacidad_personas;
+    
+    
+    
+    public void MisionEspacial(String proposito, List<String> misiones, List<String> centros_investigacion, double peso, double distancia_orbita, int capacidad_personas) {
+        this.proposito = proposito;
+        this.misiones = misiones;
+        this.centros_investigacion = centros_investigacion;
+        this.peso = peso;
+        this.distancia_orbita = distancia_orbita;
+        this.capacidad_personas = capacidad_personas;
+    }
+    
+    
+    	// gett y set de cada uno de los parametros anteriormente puestos
+    
+    public String getProposito() {
+        return proposito;
+    }
+
+    public void setProposito(String proposito) {
+        this.proposito = proposito;
+    }
+
+    public List<String> getMisiones() {
+        return misiones;
+    }
+
+    public void setMisiones(List<String> misiones) {
+        this.misiones = misiones;
+    }
+
+    public List<String> getCentros_investigacion() {
+        return centros_investigacion;
+    }
+
+    public void setCentros_investigacion(List<String> centros_investigacion) {
+        this.centros_investigacion = centros_investigacion;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getDistancia_orbita() {
+        return distancia_orbita;
+    }
+
+    public void setDistancia_orbita(double distancia_orbita) {
+        this.distancia_orbita = distancia_orbita;
+    }
+
+    public int getCapacidad_personas() {
+        return capacidad_personas;
+    }
+
+    public void setCapacidad_personas(int capacidad_personas) {
+        this.capacidad_personas = capacidad_personas;
+    }
+ // clase abstracta para las naves espaciales tripuladas
+    
+    abstract class NaveEspacialTripulada {
+    private int capacidadTripulacion;
+    private int peso;
+
+    public NaveEspacialTripulada(int capacidadTripulacion, int peso) {
+    this.capacidadTripulacion = capacidadTripulacion;
+    this.peso = peso;
+    }
+
+    public int getCapacidadTripulacion() {
+    return capacidadTripulacion;
+    }
+
+    public int getPeso() {
+    return peso;
+    }
+
+    // metodo abstracto para realizar una tarea en el espacio
+    
+    public abstract void realizarTareaEspacial();
+    }
+
+    // interfaz para naves espaciales
+    
+    interface NaveEspacialInvestigacion {
+    public void estudiarComportamientoHumano();
+    public void realizarExperimentos();
+    }
+
+    // interfaz para naves espaciales de mantenimiento
+    
+    interface NaveEspacialMantenimiento {
+    public void repararSatelites();
+    public void acoplarseConOtrasNaves();
+    public void probarEquiposElectronicos();
+    }
+}
+
+
---
+++ Parcial_3/src/naves_noTripuladas.java
@@ -0,0 +1,255 @@
+import java.util.List;
+
+public abstract class naves_noTripuladas {
+
+	public String nombre;
+	public List<String> combustible;
+	public Double empuje = 45.39;
+	public Double velocidad;
+	public Boolean activa;
+
+	
+	
+	//Metodos de saturno y sus lunas
+	
+	public void SondaEspacial(String nombre, List<String> combustible, Double empuje, Double velocidad, Boolean activa) {
+	    this.nombre = nombre;
+	    this.combustible = combustible;
+	    this.empuje = empuje;
+	    this.velocidad = velocidad;
+	    this.activa = activa;
+	}
+
+	
+	public String getNombre() {
+	    return nombre;
+	}
+
+	public void setNombre(String nombre) {
+	    this.nombre = nombre;
+	}
+
+
+	public List<String> getCombustible() {
+	    return combustible;
+	}
+
+	public void setCombustible(List<String> combustible) {
+	    this.combustible = combustible;
+	}
+
+	public Double getEmpuje() {
+	    return empuje;
+	}
+
+	public void setEmpuje(Double empuje) {
+	    this.empuje = empuje;
+	}
+
+	public Double getVelocidad() {
+	    return velocidad;
+	}
+
+	public void setVelocidad(Double velocidad) {
+	    this.velocidad = velocidad;
+	}
+
+
+	public Boolean getActiva() {
+	    return activa;
+	}
+
+	public void setActiva(Boolean activa) {
+	    this.activa = activa;
+	}
+	
+	   public abstract void desplazarse();
+	   
+	   interface ProyectoSonda {
+		    String AGENCIA_NASA = "NASA";
+		    String AGENCIA_ESA = "ESA";
+		    String AGENCIA_ASI = "ASI";
+
+		    void ejecutarProyecto();
+		    boolean estaActivo();
+		}
+	
+	
+	//Metodos de Jupiter
+	
+	public void PioneroX(String nombre, List<String> combustible, Double empuje, Double velocidad, Boolean activa) {
+		this.nombre = nombre;
+	    this.combustible = combustible;
+	    this.empuje = empuje;
+	    this.velocidad = velocidad;
+	    this.activa = activa;
+	}
+
+	
+	public String getNombre1() {
+	    return nombre;
+	}
+
+	public void setNombre1(String nombre) {
+	    this.nombre = nombre;
+	}
+
+
+	public List<String> getCombustible1() {
+	    return combustible;
+	}
+
+	public void setCombustible1(List<String> combustible) {
+	    this.combustible = combustible;
+	}
+
+	public Double getEmpuje1() {
+	    return empuje;
+	}
+
+	public void setEmpuje1(Double empuje) {
+	    this.empuje = empuje;
+	}
+
+	public Double getVelocidad1() {
+	    return velocidad;
+	}
+
+	public void setVelocidad1(Double velocidad) {
+	    this.velocidad = velocidad;
+	}
+
+
+	public Boolean getActiva1() {
+	    return activa;
+	}
+
+	public void setActiva1(Boolean activa) {
+	    this.activa = activa;
+	}
+	
+	
+	public void PioneroXI(String nombre, List<String> combustible, Double empuje, Double velocidad, Boolean activa) {
+		this.nombre = nombre;
+	    this.combustible = combustible;
+	    this.empuje = empuje;
+	    this.velocidad = velocidad;
+	    this.activa = activa;
+	}
+		
+		public String getNombre2() {
+		    return nombre;
+		}
+
+		public void setNombre2(String nombre) {
+		    this.nombre = nombre;
+		}
+
+
+		public List<String> getCombustible2() {
+		    return combustible;
+		}
+
+		public void setCombustible2(List<String> combustible) {
+		    this.combustible = combustible;
+		}
+
+		public Double getEmpuje2() {
+		    return empuje;
+		}
+
+		public void setEmpuje2(Double empuje) {
+		    this.empuje = empuje;
+		}
+
+		public Double getVelocidad2() {
+		    return velocidad;
+		}
+
+		public void setVelocidad2(Double velocidad) {
+		    this.velocidad = velocidad;
+		}
+
+
+		public Boolean getActiva2() {
+		    return activa;
+		}
+
+		public void setActiva2(Boolean activa) {
+		    this.activa = activa;
+		}
+		
+		// Metodo para pluton
+		
+		public void NewHorizon(String nombre, List<String> combustible, Double empuje, Double velocidad, Boolean activa) {
+			this.nombre = nombre;
+		    this.combustible = combustible;
+		    this.empuje = empuje;
+		    this.velocidad = velocidad;
+		    this.activa = activa;
+		}
+			
+			public String getNombre3() {
+			    return nombre;
+			}
+
+			public void setNombre3(String nombre) {
+			    this.nombre = nombre;
+			}
+
+
+			public List<String> getCombustible3() {
+			    return combustible;
+			}
+
+			public void setCombustible3(List<String> combustible) {
+			    this.combustible = combustible;
+			}
+
+			public Double getEmpuje3() {
+			    return empuje;
+			}
+
+			public void setEmpuje3(Double empuje) {
+			    this.empuje = empuje;
+			}
+
+			public Double getVelocidad3() {
+			    return velocidad;
+			}
+
+			public void setVelocidad3(Double velocidad) {
+			    this.velocidad = velocidad;
+			}
+
+
+			public Boolean getActiva3() {
+			    return activa;
+			}
+
+			public void setActiva3(Boolean activa) {
+			    this.activa = activa;
+			}
+			
+			public interface Motor {
+				   public double getEmpuje();
+				}
+			public abstract class SondaPluton extends naves_noTripuladas {
+				   private String objetivo;
+
+				   public void setObjetivo(String objetivo) {
+				      this.objetivo = objetivo;
+				   }
+
+				   public String getObjetivo() {
+				      return objetivo;
+				   }
+
+				   public abstract void analizarObjetivo();
+				}
+}
+
+	
+
+	
+
